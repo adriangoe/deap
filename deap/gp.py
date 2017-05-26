1afe8b643a1f853946948ad5d8d23015141bb47b
Adrian Goedeckemeyer <adrian@minerva.kgi.edu>
2017-05-26 17:14:34 +0200

    Relax height req if no primitive meets type requirement

--- deap/gp.py
+++ deap/gp.py
@@ -580,7 +580,6 @@ def generate(pset, min_, max_, condition, type_=None):
     """Generate a Tree as a list of list. The tree is build
     from the root to the leaves, and it stop growing when the
     condition is fulfilled.
-
     :param pset: Primitive set from which primitives are selected.
     :param min_: Minimum height of the produced trees.
     :param max_: Maximum Height of the produced trees.
@@ -592,6 +591,41 @@ def generate(pset, min_, max_, condition, type_=None):
                   is assumed.
     :returns: A grown tree with leaves at possibly different depths
               dependending on the condition function.
+
+    DUMMY NODE ISSUES
+
+    DEAP will only place terminals if we're at the bottom of a branch.
+    This creates two issues:
+    1. A primitive that takes other primitives as inputs could be placed at the
+        second to last layer.
+        SOLUTION: You need to allow the tree to end whenever the height condition is met,
+                    so create "dummy" terminals for every type possible in the tree.
+    2. A primitive that takes terminals as inputs could be placed above the second to
+        last laycPer.
+        SOLUTION: You need to allow the tree to continue extending the branch until the
+                    height condition is met, so create "dummy" primitives that just pass
+                    through the terminal types.
+                    cc
+    These "dummy" terminals and "dummy" primitives introduce unnecessary and sometimes
+    nonsensical solutions into populations. These "dummy" nodes can be eliminated
+    if the height requirement is relaxed.
+
+
+    HOW TO PREVENT DUMMY NODE ISSUES
+
+    Relaxing the height requirement:
+    When at the bottom of the branch, check for terminals first, then primitives.
+        When checking for primitives, skirt the height requirement by adjusting
+        the branch depth to be the second to last layer of the tree.
+        If neither a terminal or primitive fits this node, then throw an error.
+    When not at the bottom of the branch, check for primitives first, then terminals.
+
+    Issue with relaxing the height requirement:
+    1. Endless loops are possible when primitive sets have any type loops.
+        A primitive with an output of one type may not take an input type of
+        itself or a parent type.
+        SOLUTION: A primitive set must be well-designed to prevent those type loops.
+
     """
     if type_ is None:
         type_ = pset.ret
@@ -600,28 +634,58 @@ def generate(pset, min_, max_, condition, type_=None):
     stack = [(0, type_)]
     while len(stack) != 0:
         depth, type_ = stack.pop()
+        # At the bottom of the tree
         if condition(height, depth):
+            # Try finding a terminal
             try:
                 term = random.choice(pset.terminals[type_])
-            except IndexError:
-                _, _, traceback = sys.exc_info()
-                raise IndexError, "The gp.generate function tried to add "\
-                                  "a terminal of type '%s', but there is "\
-                                  "none available." % (type_,), traceback
-            if isclass(term):
-                term = term()
-            expr.append(term)
+
+                if isclass(term):
+                    term = term()
+                expr.append(term)
+            # No terminal fits
+            except:
+                # So pull the depth back one layer, and start looking for primitives
+                try:
+                    depth -= 1
+                    prim = random.choice(pset.primitives[type_])
+
+                    expr.append(prim)
+                    for arg in reversed(prim.args):
+                        stack.append((depth+1, arg))
+
+                # No primitive fits, either - that's an error
+                except IndexError:
+                    _, _, traceback = sys.exc_info()
+                    raise IndexError, "The gp.generate function tried to add "\
+                                      "a primitive of type '%s', but there is "\
+                                      "none available." % (type_,), traceback
+
+        # Not at the bottom of the tree
         else:
+            # Check for primitives
             try:
                 prim = random.choice(pset.primitives[type_])
-            except IndexError:
-                _, _, traceback = sys.exc_info()
-                raise IndexError, "The gp.generate function tried to add "\
-                                  "a primitive of type '%s', but there is "\
-                                  "none available." % (type_,), traceback
-            expr.append(prim)
-            for arg in reversed(prim.args):
-                stack.append((depth + 1, arg))
+
+                expr.append(prim)
+                for arg in reversed(prim.args):
+                    stack.append((depth+1, arg))
+            # No primitive fits
+            except:
+                # So check for terminals
+                try:
+                    term = random.choice(pset.terminals[type_])
+
+                # No terminal fits, either - that's an error
+                except IndexError:
+                    _, _, traceback = sys.exc_info()
+                    raise IndexError, "The gp.generate function tried to add "\
+                                      "a terminal of type '%s', but there is "\
+                                      "none available." % (type_,), traceback
+                if isclass(term):
+                    term = term()
+                expr.append(term)
+
     return expr
 
 
